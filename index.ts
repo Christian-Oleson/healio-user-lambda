847f7d18f7d143e71c951f07b05ad0c3a9c77973
Christian Oleson <olesoncs@gmail.com>
2025-05-31 07:48:16 -0500

    HS User Lambda - Update Link Params

--- index.ts
+++ index.ts
@@ -5,9 +5,12 @@ export interface CognitoEvent {
   triggerSource: string;
   request: { 
     codeParameter: string;
+    linkParameter?: string;
     usernameParameter?: string;
     userAttributes?: {
       email: string;
+      family_name?: string;
+      given_name?: string | null;
       [key: string]: string;
     }
   };
