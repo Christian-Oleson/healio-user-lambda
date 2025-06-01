8331e19db502d4dc64efd0d67948a6fef8b73140
Christian Oleson <olesoncs@gmail.com>
2025-06-01 14:13:10 -0500

    HS User Lambda - Add Sentry to Lambda

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
+import * as Sentry from "@sentry/aws-serverless"
+
+Sentry.init({
+  dsn: "https://e5c2b4c7c153d689465a4406a5855b51@o4508237537869824.ingest.us.sentry.io/4509425005494272",
+
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
 import { APP_NAME } from './config.js';
 import { adminCreateUserEmail } from './emailTemplates.js';
 import { CognitoEvent } from './interfaces/cognitoEvent.js';
@@ -6,6 +16,24 @@ import { AdminCreateUserStrategy } from './strategies/adminCreateUserStrategy.js
 import { ForgotPasswordStrategy } from './strategies/forgotPasswordStrategy.js';
 import { PreSignUpStrategy } from './strategies/preSignUpStrategy.js';
 import { SignUpStrategy } from './strategies/signUpStrategy.js';
+
+exports.handler = Sentry.wrapHandler(async (event: CognitoEvent, context) => {
+  // Your handler code
+  console.log('Event:', JSON.stringify(event, null, 2));
+
+  try {
+    const strategyFactory = new TriggerStrategyFactory();
+    const strategy = strategyFactory.getStrategy(event.triggerSource);
+    event = await strategy.handle(event);
+  } catch (error) {
+    console.error('Error processing event:', error);
+    console.log('Event:', JSON.stringify(event, null, 2));
+  }
+
+  console.log('---------------output----------------');
+  console.log('Event:', JSON.stringify(event, null, 2));
+  return event;
+});
 
 // Strategy factory to get the appropriate handler for each trigger type
 class TriggerStrategyFactory {
@@ -39,21 +67,3 @@ class TriggerStrategyFactory {
     };
   }
 }
-
-export const handler = async (event: CognitoEvent): Promise<CognitoEvent> => {
-  console.log('Event:', JSON.stringify(event, null, 2));
-
-  try {
-    const strategyFactory = new TriggerStrategyFactory();
-    const strategy = strategyFactory.getStrategy(event.triggerSource);
-    event = await strategy.handle(event);
-  } catch (error) {
-    console.error('Error processing event:', error);
-    console.log('Event:', JSON.stringify(event, null, 2));
-  }
-
-  console.log('---------------output----------------');
-  console.log('Event:', JSON.stringify(event, null, 2));
-  return event;
-};
-
