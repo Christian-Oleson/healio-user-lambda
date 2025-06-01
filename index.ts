13de033904b889e9ec159b061737a0a91845c386
Christian Oleson <olesoncs@gmail.com>
2025-06-01 14:21:37 -0500

    HS User Lambda - Remove Strategy that is failing

--- index.ts
+++ index.ts
@@ -26,7 +26,6 @@ class TriggerStrategyFactory {
     this.strategies.set('CustomMessage_SignUp', new SignUpStrategy());
     this.strategies.set('CustomMessage_ForgotPassword', new ForgotPasswordStrategy());
     this.strategies.set('CustomMessage_AdminCreateUser', new AdminCreateUserStrategy());
-    this.strategies.set('PreSignUp_SignUp', new PreSignUpStrategy());
   }
 
   getStrategy(triggerSource: string): TriggerStrategy {
